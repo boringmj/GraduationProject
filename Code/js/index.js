c9c36c6cba6a36e5d19c1395f5649057422fe6d5
boringmj <2878663643@qq.com>
2021-12-04 07:45:53 +0800

    首页定稿

--- Code/js/index.js
+++ Code/js/index.js
@@ -1,11 +1,80 @@
+//定于全局初始变量
+var commodity={"page":2,"limit":4,"active":1,"type":"shoe","data_rul":"./data/commodity.json","img_dir":"./images/commodity","commodity_data":{}};
+
+//预处理事件(部分数据是不需要实时更新的,只需要处理一次就可以了)
+$.getJSON(commodity.data_rul,"", function(data){
+    commodity.commodity_data=data;
+});
+
 //监听商品换页按钮移入移出事件
 $("#commodity_show\\-go_left").hover(function(){
-    $("#commodity_show\\-go_left").attr("src","./images/go_left_active.png");
+    $(this).attr("src","./images/go_left_active.png");
 },function(){
-    $("#commodity_show\\-go_left").attr("src","./images/go_left.png");
+    $(this).attr("src","./images/go_left.png");
 });
 $("#commodity_show\\-go_right").hover(function(){
-    $("#commodity_show\\-go_right").attr("src","./images/go_right_active.png");
+    $(this).attr("src","./images/go_right_active.png");
 },function(){
-    $("#commodity_show\\-go_right").attr("src","./images/go_right.png");
+    $(this).attr("src","./images/go_right.png");
 });
+
+//监听商品换页按钮点击事件
+$("#commodity_show\\-go_left").click(function(){
+    if(commodity.active<=1) return 0;
+    commodity.active--;
+    show_commodity_list();
+});
+$("#commodity_show\\-go_right").click(function(){
+    if(commodity.active>=commodity.page) return 0;
+    commodity.active++;
+    show_commodity_list();
+});
+
+//监听切换商品列表按钮
+$(".commodity-type").click(function(){
+    commodity.type=$(this).attr('commodity-type');
+    commodity.active=1;
+    $(".commodity-type").each(function(){
+        $(this).removeClass("active");
+    });
+    $(this).addClass("active");
+    show_commodity_list();
+});
+
+//定义一个用于刷新当前商品列表的函数
+function show_commodity_list(){
+    var commodity_page=$("#commodity_page");
+    var commodity_data={};
+    if(commodity.type=="shoe") commodity_data=commodity.commodity_data.shoe;
+    else if(commodity.type=="clothing") commodity_data=commodity.commodity_data.clothing;
+    else if(commodity.type=="children_clothing") commodity_data=commodity.commodity_data.children_clothing;
+    else commodity_data=commodity.commodity_data.other;
+    commodity.page=commodity_data.page;
+    commodity_data_active=commodity_data.data[commodity.active-1];
+    var i=0;
+    $("#commodity_list img").each(function(){
+        $(this).attr("src",commodity.img_dir+"/"+commodity_data_active[i][3]);
+        i++;
+    });
+    i=0;
+    $("#commodity_list .font-commodity-name").each(function(){
+        $(this).html(commodity_data_active[i][0]);
+        i++;
+    });
+    i=0;
+    $("#commodity_list .font-commodity-info").each(function(){
+        $(this).html(commodity_data_active[i][1]);
+        i++;
+    });
+    i=0;
+    $("#commodity_list .font-commodity-price").each(function(){
+       $(this).html("&#165;"+commodity_data_active[i][2]);
+        i++;
+    });
+    commodity_page.empty();
+    for(i=0;i<commodity.page;i++)
+    {
+        if(i==commodity.active-1) commodity_page.append("<a href=\"javascript:void(0);\"><span class=\"active\"></span></a>");
+        else commodity_page.append("<a href=\"javascript:void(0);\"><span></span></a>");
+    }
+}

23e1d4139f981c36d0a7b578aae9604851065592
boringmj <2878663643@qq.com>
2021-12-10 21:31:00 +0800

    购物车交互-初稿

---
+++ Code/js/cart.js
@@ -0,0 +1,52 @@
+//定义全局对象
+cart={"checked":{"true":"./images/cart-checked.png","false":"./images/cart-unchecked.png"},"concessions":20}
+
+//选择组件监听
+$(".checkbox").click(function(){
+    checked=$(this).attr("commodity_checked");
+    //判断被点击的是否为全选按钮
+    if($(this).hasClass("checkbox-all"))
+    {
+        $(".commodity_list .info .checkbox").each(function(){
+            if(checked=="true")
+            {
+                $(this).attr("src",cart.checked.false);
+                $(this).attr("commodity_checked","false");
+            }
+            else
+            {
+                $(this).attr("src",cart.checked.true);
+                $(this).attr("commodity_checked","true");
+            }
+        });
+    }
+    //改变被被点击按钮的属性
+    if(checked=="true")
+    {
+        $(this).attr("src",cart.checked.false);
+        $(this).attr("commodity_checked","false");
+    }
+    else
+    {
+        $(this).attr("src",cart.checked.true);
+        $(this).attr("commodity_checked","true");
+    }
+    show_commodity_info();
+});
+
+//定义一个显示商品价格的函数
+function show_commodity_info()
+{
+    var count=0,i=0;
+    $(".commodity_list .info .checkbox").each(function(){
+        checked=$(this).attr("commodity_checked");
+        if(checked=="true")
+        {
+            count+=Number($(this).attr("money_value"));
+            i++;
+        }
+    });
+    $(".stats_commodity_money").html("&#165;"+count);
+    $(".stats-concessions").html("&#165;"+(cart.concessions*i));
+    $(".stats-count").html("&#165;"+(count-cart.concessions*i));
+}

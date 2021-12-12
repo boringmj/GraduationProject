d84d2dec08545f74073e85437c9138beccfa9bc0
boringmj <2878663643@qq.com>
2021-12-12 21:25:11 +0800

    我的-初稿

---
+++ Code/js/user.js
@@ -0,0 +1,48 @@
+//定义与id参数对应的控件id
+var user={
+    "id":[
+        ["#v-tnav-tabs-info-tab","#v-tnav-tabs-info","info"],
+        ["#v-tnav-tabs-collect-tab","#v-tnav-tabs-collect","collect"],
+        ["#v-tnav-tabs-address-tab","#v-tnav-tabs-address","address"],
+        ["#v-tnav-tabs-coupon-tab","#v-tnav-tabs-coupon","coupon"],
+        ["#v-tnav-tabs-order-tab","#v-tnav-tabs-order","order"],
+        ["#v-tnav-tabs-after_sale-tab","#v-tnav-tabs-after_sale","after_sale"]
+    ],
+    "default":0
+};
+
+var id=getQueryVariable("id");
+if(id)
+{
+    var count=user.id.length;
+    for(var i=0;i<count;i++)
+    {
+        if(id==user.id[i][2])
+        {
+            $(user.id[user.default][0]).removeClass("active");
+            $(user.id[user.default][1]).removeClass("active");
+            $(user.id[user.default][1]).removeClass("show");
+            $(user.id[user.default][0]).attr("aria-selected","false");
+            $(user.id[i][0]).addClass("active");
+            $(user.id[i][1]).addClass("active");
+            $(user.id[i][1]).addClass("show");
+            $(user.id[i][0]).attr("aria-selected","true");
+        }
+    }
+}
+
+//定义一个用于获取get参数的函数
+function getQueryVariable(variable)
+{
+    var query=window.location.search.substring(1);
+    var vars=query.split("&");
+    for(var i=0;i<vars.length;i++)
+    {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable)
+        {
+            return pair[1];
+        }
+    }
+    return(false);
+}

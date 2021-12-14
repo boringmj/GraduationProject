3d46edd5e761779da99f1d1c560d255cbaf7e62b
boringmj <2878663643@qq.com>
2021-12-14 15:03:59 +0800

    商品_更多-初稿

---
+++ Code/js/merchandise_more.js
@@ -0,0 +1,48 @@
+//绑定地址与参数(第一个为默认)
+var merchandise_value=[
+    ["clothing","./data/merchandise_more_clothing.json"],
+    ["shoe","./data/merchandise_more_shoe.json"],
+    ["children","./data/merchandise_more_children.json"]
+];
+
+var get_value=getQueryVariable("id");
+var is_value=false;
+var merchandise_path;
+if(get_value)
+    for(i in merchandise_value)
+        if(merchandise_value[i][0]==get_value)
+        {
+            merchandise_path=merchandise_value[i][1];
+            is_value=true;
+        }
+if(!is_value)
+    merchandise_path=merchandise_value[0][1];
+$.getJSON(merchandise_path,"",function(data){
+    var dir_path=data.img_path+"/"+data.id+"/";
+    $(".title\-en").html(data.title_en);
+    $(".title\-zh").html(data.title_zh);
+    $(".merchandise_banner").attr("src",dir_path+data.banner_img);
+    var i=0;
+    $(".info-content").each(function(){
+        $($(this).find(".info\-content\-01")).text(data.data[i][0]);
+        $($(this).find(".info_images")).attr("src",dir_path+data.data[i][1]);
+        $($(this).find(".info\-content\-02")).html(data.data[i][2]);
+        i++;
+    });
+});
+
+//定义一个用于获取get参数的函数
+function getQueryVariable(variable)
+{
+    var query=window.location.search.substring(1);
+    var vars=query.split("&");
+    for(var i=0;i<vars.length;i++)
+    {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable)
+        {
+            return pair[1];
+        }
+    }
+    return(false);
+}
